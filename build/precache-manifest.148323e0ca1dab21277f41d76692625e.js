70b18e72991d0cc248096de89786fd3aa4d5c50b
Shaurya Mittal <shauryamittal5@gmail.com>
2021-05-12 10:34:08 -0700

    pagesURL

---
+++ build/precache-manifest.148323e0ca1dab21277f41d76692625e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "114ebd935e4e82a4cf48",
+    "url": "/shamit2008/SecurityAppPages/home/static/js/runtime~main.1efa0bee.js"
+  },
+  {
+    "revision": "fdbf6ae0e27a4361a51b",
+    "url": "/shamit2008/SecurityAppPages/home/static/js/main.75bd8564.chunk.js"
+  },
+  {
+    "revision": "7e08248b95aa29b3e1da",
+    "url": "/shamit2008/SecurityAppPages/home/static/js/2.3b146fbb.chunk.js"
+  },
+  {
+    "revision": "fdbf6ae0e27a4361a51b",
+    "url": "/shamit2008/SecurityAppPages/home/static/css/main.e12a1601.chunk.css"
+  },
+  {
+    "revision": "ed2898297b130b1794bcbc31b6947fe4",
+    "url": "/shamit2008/SecurityAppPages/home/index.html"
+  }
+];
